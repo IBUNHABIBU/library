a7cf4f8c98384d4e268767bf4bf228c8649be728
IBUNHABIBU <ibunhabib@gmail.com>
2020-07-09 18:20:11 +0300

    adding saver class

--- library.js
+++ library.js
@@ -1,9 +1,8 @@
-let bookList = []
+let bookList = [];
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const pages = document.getElementById('pages');
 const addBook = document.getElementById('book-form');
-
 class Book{
     constructor(title,author,pages){
         this.title = title;
@@ -11,31 +10,53 @@ class Book{
         this.pages = pages;
     }
 }
-addBook.addEventListener('submit',(e)=>{
-    e.preventDefault()
-    const bookTitle = title.value;
-    const bookAuthor = author.value;
-    const bookPages = pages.value;
-    const book = new Book(bookTitle,bookAuthor,bookPages);
-    bookList.push(book);
-    console.log(bookList);
-});
+class Saver {
+    static getBooks(){
+        let books;
+        books = localStorage.getItem('books')===null ? [] : JSON.parse(localStorage.getItem('books'));
+        return books;
+    }
+    static addBook(book){
+        const books = Saver.getBooks();
+        books.push(book);
+        localStorage.setItem('books',JSON.stringify(books));
+    }
+    static removeBook(title){
+        const book = Saver.getBooks();
+        books.forEach((element,index) => {
+            if (element.title==title){
+                element.splice(index,1);
+            }
+        });
+        localStorage.setItem('books',JSON.stringify(books));
+    }
+}
+
 class UserDisplay{
     static addBookToLibrary(book) {
+       
         const bookDetails = document.getElementById('book-details');
         const tableRow = document.createElement('tr');
         tableRow.innerHTML = `
                                 <td>${book.title}</td>
                                 <td>${book.author}</td>
                                 <td>${book.pages}</td>
+                                <td><a href="#" class="btn btn-danger btn-sm delete">Delete Book</a></td>
         `
         bookDetails.appendChild(tableRow);
     }
     static displayBook(){
-
+        const books = Saver.getBooks();
+        books.forEach(element=>UserDisplay.addBookToLibrary(element))
     }
-}
-
-class LocalStore {
-    
+    static deleteBook(element){
+        if(element.classList.contains('delete')){
+            element.parentElement.parentElement.remove();
+        }
+    }
+    static clearField(){
+         title.value = ' ';
+         author.value =  ' ';
+         pages.value = ' ';
+    }
 }
