3411bef2f380c1aec546732edddff2788e8b5537
IBUNHABIBU <ibunhabib@gmail.com>
2020-07-14 17:58:21 +0300

    removing lint errors

--- library.js
+++ library.js
@@ -112,23 +112,23 @@ class Validate {
     }, 3000);
   }
 
-static  getFieldName(input) {
+  static  getFieldName(input) {
   return input.id.charAt(0).toUpperCase()+input.id.slice(1)
-}
-    
-static showAlert(message, className) {
-  const div = document.createElement('div');
-  div.className = `alert alert-${className} alert-success-style `;
-  div.style.zIndex = '10';
-  div.appendChild(document.createTextNode(message));
-  const container = document.querySelector('.header-container');
-  const form = document.querySelector('#book-form');
-  container.insertBefore(div, form);
-    setTimeout(() => {
-      document.querySelector('.alert').remove();
-    }, 3000);
   }
-}
+    
+  static showAlert(message, className) {
+    const div = document.createElement('div');
+    div.className = `alert alert-${className} alert-success-style `;
+    div.style.zIndex = '10';
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.header-container');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+      setTimeout(() => {
+        document.querySelector('.alert').remove();
+      }, 3000);
+    }
+  }
 document.addEventListener('DOMContentLoaded', UserDisplay.renderBook);
 
 document.getElementById('book-form').addEventListener('submit', (e) => {
